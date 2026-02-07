30002f46d83e6499117dcd46ddd9f11e4b49eb23
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2026-02-07 18:38:35 -0500

    Add Aaron's final Embedder model to model.py

--- ml_pipeline/src/model.py
+++ ml_pipeline/src/model.py
@@ -46,6 +46,61 @@ class EmbedderGNNv2(torch.nn.Module):
         self.convs.append(SAGEConv(hidden_dim, out_dim, aggr='mean'))
         self.bns.append(BatchNorm(out_dim))
 
+class EmbedderGNNv3(torch.nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim, num_layers=3, dropout=0.5):
+        super().__init__()
+        self.dropout = dropout
+        self.num_layers = num_layers
+
+        self.convs = torch.nn.ModuleList()
+        self.bns = torch.nn.ModuleList()
+
+        # Layer 1: Input -> Hidden
+        self.convs.append(SAGEConv(in_dim, hidden_dim, aggr='mean'))
+        self.bns.append(BatchNorm(hidden_dim))
+
+        # Learnable Mask Token (Replacing Node 0's features)
+        self.mask_embed = torch.nn.Parameter(torch.randn(in_dim), requires_grad=True)
+        # self.register_parameter("mask_embed", self.mask_embed) # Not strictly needed if assigned to self
+
+        # Hidden Layers
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden_dim, hidden_dim, aggr='mean'))
+            self.bns.append(BatchNorm(hidden_dim))
+
+        # Output Layer: Hidden -> Out
+        self.convs.append(SAGEConv(hidden_dim, out_dim, aggr='mean'))
+        self.bns.append(BatchNorm(out_dim))
+
+    def forward(self, x, edge_index):
+        # 1. Masking Strategy
+        # We clone x so we don't modify the original dataset in memory
+        # 'detach()' stops gradients flowing back to raw features (which are fixed anyway)
+        x = x.clone().detach()
+        
+        # Replace the first node in the batch with the learnable mask token
+        # Note: In LinkNeighborLoader, node 0 is usually a target node for the batch
+        x[0] = self.mask_embed
+
+        # 2. Message Passing Loop
+        for i in range(self.num_layers):
+            h = self.convs[i](x, edge_index)
+            h = self.bns[i](h)
+        
+            # Activation & Dropout (except for last layer)
+            if i != self.num_layers - 1:
+                h = F.relu(h)
+                h = F.dropout(h, p=self.dropout, training=self.training)
+
+            # Residual Connection
+            # Only apply if shapes match (Input Dim != Hidden Dim on layer 0)
+            if x.shape == h.shape:
+                h = h + x
+            
+            x = h
+
+        return x
+
     # Input format (probably): 
     # x: (n, F)
     # edge_index: (2, E)
